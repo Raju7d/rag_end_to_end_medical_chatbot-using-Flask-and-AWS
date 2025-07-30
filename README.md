3a75d90275f7f92d2059b063432498b8216303d9
Raju Veerla <rajuveerla98@gmail.com>
2025-07-31 00:38:36 +0530

    updated test pinecone client

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 
 # step 1
 1. Create a template.sh file and write the directory over there
+2. Copy the Medical book to data folder
+3. 
--- template.sh
+++ template.sh
@@ -1,19 +1,19 @@
-# create a folder structure using shell script
+# Creating directory 
 mkdir -p src
-mkdir -p research
-mkdir -p data
+mkdir -p research 
 
-# create files
+# Creating files
 touch src/__init__.py
-touch src/helper.py
+touch src/helper.py 
 touch src/prompt.py
-touch .env
+touch .env 
 touch setup.py
 touch app.py
-touch research/trails.ipynb
+touch research/trials.ipynb 
 touch requirements.txt
 
-echo "Directory and files structure created successfully"
+
+echo "Directory and files created successfully!."
 
 # Run command
 # Open Gibash in vscode and run the below command
